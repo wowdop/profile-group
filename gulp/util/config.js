4f01bb32f8fd1e55dd6eeb4a2b833e9fb96e8e44
Antonia Mihai <mihai_antoniamaria@yahoo.com>
2022-09-05 17:26:55 +0300

    feat: am adaugat 2 fiiere audio #56

--- gulp/util/config.js
+++ gulp/util/config.js
@@ -1,6 +1,6 @@
 const config = {
   fileExpression: {
-    copy: '**/*.{doc,docx,zip,pdf,js,mov,mp4}',
+    copy: '**/*.{doc,docx,zip,pdf,js,mov,mp4,mp3}',
     font: '**/*.{otf,eot,svg,ttf,woff,woff2}',
     image: '**/*.{jpg,jpeg,gif,svg,png,JPG,JPEG,GIF,SVG,PNG}',
     ico: './*.{ico,json}'
